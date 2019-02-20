c2735b94d453938f27a91e553886078cfa1f38af
Bulat <hisaev@gmail.com>
2019-02-20 21:45:47 +0300

    исправил форму ввода

--- js/main.js
+++ js/main.js
@@ -321,8 +321,3 @@ function validateField(field) {
         return true;
     }
 }
-
-function numberOrder(){
-    if (event.keyCode != 43 && event.keyCode < 48 || event.keyCode > 57)
-    event.preventDefault();
-}
